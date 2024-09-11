566b1d0cf0e6eb171bc7d3ab2409ea8412ac7cd1
Ashu Singh <ashu369420@gmail.com>
2024-09-11 16:01:03 +0530

    create redux setup and generate custom file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './auth/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL,'VITE_APPWRITE_URL')
-  return (
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login({ user }))
+        } else {
+          dispatch(logout())
+        }
+        console.log(user)
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ?
     <>
-      <h1>A blog with app write</h1>
-    </>
-  )
+      <div className='min-h-full flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+
+      </div>
+
+    </> :
+    <></>
 }
 
 export default App
