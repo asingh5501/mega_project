f83d2c0404a144bad2c826a103de9dd8d626b42b
Ashu Singh <ashu369420@gmail.com>
2024-09-12 12:37:54 +0530

    design custom buttom and input field

---
+++ src/components/Header/Input.jsx
@@ -0,0 +1,24 @@
+import React, { useId, forwardRef } from 'react'
+
+const Input = forwardRef(function Input({
+    label,
+    type = "text",
+    className = "",
+    ...props,
+}, ref) {
+    const id = useId()
+    return (
+        <div className='w-full'>{
+            label && <label htmlFor={id} className='inline-block mb-1 pb-1'></label>
+            <input
+                type={type}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                key={id}
+                {...props}
+            />
+        }</div>
+    )
+})
+
+export default Input
